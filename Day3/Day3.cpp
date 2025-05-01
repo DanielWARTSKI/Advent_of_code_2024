600b4a17d82b183c2edc01f7884606d913e605db
DanielWARTSKI <146826868+DanielWARTSKI@users.noreply.github.com>
2025-05-02 00:59:48 +0200

    Update Day3.cpp

--- Day3/Day3.cpp
+++ Day3/Day3.cpp
@@ -10,11 +10,6 @@ int main() {
     bool enabled = 1;
 
     std::ifstream file("input3.txt");
-    /*if (!file) {
-        std::cerr << "Erreur d'ouverture du fichier." << std::endl;
-        return 1;
-    }*/
-    //std::cout << "Fichier ouvert avec succès." << std::endl;
 
     //std::regex pattern(R"(mul\((\d{1,3}),(\d{1,3})\))");
     std::regex pattern_do_dont(R"(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))");
@@ -22,32 +17,10 @@ int main() {
     std::smatch matches;
 
     while (std::getline(file, line)) {
-        //std::cout << "Ligne lue : " << line << std::endl; // DEBUG
+        //std::cout << "Ligne lue : " << line << std::endl;
         auto begin = line.cbegin();
         auto end = line.cend();
-        //auto begin_v2 = begin;
 
-
-        /*while (std::regex_search(begin, end, matches, pattern)) {
-            //std::cout << "Détection : " << matches[0] << " (X=" << matches[1] << ", Y=" << matches[2] << ")" << std::endl;
-            int x = std::stoi(matches[1].str());
-            int y = std::stoi(matches[2].str());
-            total += x * y;
-            begin = matches.suffix().first; // continuer à chercher sur le reste de la ligne
-        }
-        while (std::regex_search(begin_v2, end, matches, pattern_do_dont)) {
-            std::string instruction = matches[0];
-            if (instruction == "do()") {
-                enabled = 1;
-            } else if (instruction == "don't()") {
-                enabled = 0;
-            } else if (matches[1].matched && matches[2].matched && enabled) {
-                int x = std::stoi(matches[1]);
-                int y = std::stoi(matches[2]);
-                total_v2 += x * y;
-            }
-            begin_v2 = matches.suffix().first;
-        }*/
         while (std::regex_search(begin, end, matches, pattern_do_dont)) {
             std::string instruction = matches[0];
             if (instruction == "do()") {
